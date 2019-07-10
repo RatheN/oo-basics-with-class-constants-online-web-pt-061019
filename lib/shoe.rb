d274ef086ea9aa00bd2d3fde8b098800aa1794af
Nathan Rathe <nprathe10@gmail.com>
2019-07-10 17:36:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
         BRANDS << brand
-        if BRANDS.include?(brand)
-          brand.delete
+        brand.deleteif BRANDS.include?(brand)
+          
         end
   end
 
